a35364c7ee3f51899582f6865a07bd789ce3cad2
motoshima1150 <motonari.nakashima@quipper.com>
2020-08-12 14:10:25 +0900

    Initial Commit

---
+++ TDDSample/TDDSample.h
@@ -0,0 +1,19 @@
+//
+//  TDDSample.h
+//  TDDSample
+//
+//  Created by Motonari NAKASHIMA on 2020/08/12.
+//  Copyright © 2020 Motonari NAKASHIMA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TDDSample.
+FOUNDATION_EXPORT double TDDSampleVersionNumber;
+
+//! Project version string for TDDSample.
+FOUNDATION_EXPORT const unsigned char TDDSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDDSample/PublicHeader.h>
+
+
