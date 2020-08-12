ea9fed50d53c55e18260a15c868fe4ff7f17f061
motoshima1150 <motonari.nakashima@quipper.com>
2020-08-12 14:14:49 +0900

    Describe a test case that fails

---
+++ TDDSample/InstrumentCalculator.swift
@@ -0,0 +1,19 @@
+//
+//  InstrumentCalculator.swift
+//  TDDSample
+//
+//  Created by Motonari NAKASHIMA on 2020/08/12.
+//  Copyright © 2020 Motonari NAKASHIMA. All rights reserved.
+//
+
+import Foundation
+
+final class InstrumentCalculator {
+    func firstMomentAbout(element: Double) -> Double {
+        return Double.nan
+    }
+
+    func add(element: Double) {
+
+    }
+}
